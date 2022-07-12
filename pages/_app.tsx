7c902e534bfe1dcb04ccb579cc6631d6ccd6ab77
Felipe Salazar <pipelon54@gmail.com>
2022-07-12 22:55:42 +0000

    Add canonical urls

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const canonicalUrl = (`https://mañanaynoche.com` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+  return (
+    <>
+      <DefaultSeo canonical={canonicalUrl} />
+      <Component {...pageProps} />
+    </>
+  )
+
 }
