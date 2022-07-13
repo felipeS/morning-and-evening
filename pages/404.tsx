7497b8af9ee87a4b136e441fcd2ef3321880bacf
Felipe Salazar <pipelon54@gmail.com>
2022-07-13 22:14:09 +0000

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+import Container from "../components/container"
+import HeroPost from "../components/hero-post"
+import Intro from "../components/intro"
+import Layout from "../components/layout"
+import MoreStories from '../components/more-stories'
+import { PostType } from "../interfaces/post"
+import { getAllPosts } from "../lib/api"
+
+type Props = {
+  allPosts: PostType[]
+}
+
+export default function Custom404({allPosts}: Props) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Página no encontrada | Mañana y Noche por Charles Spurgeon</title>
+        </Head>
+        <Container>
+          <h1>Página no encontrada</h1>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { DefaultSeo } from 'next-seo';
-import App, { AppContext, AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import { useRouter } from 'next/router';
 import '../styles/index.css'
 
@@ -12,17 +12,4 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </>
   )
-}
-
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext)
-
-  if (appContext.ctx.res?.statusCode === 404) {
-    appContext.ctx.res.writeHead(302, { Location: '/' })
-    appContext.ctx.res.end()
-    return
-  }
-
-  return { ...appProps }
 }
