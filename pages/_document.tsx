99a102bcfef8dabaa82a9a1d46afb71ff016ac5a
Felipe Salazar <pipelon54@gmail.com>
2022-07-17 00:31:30 +0000

    Add plasuible analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script defer data-domain="xn--maanaynoche-2db.com" src="https://plausible.io/js/plausible.js"></script>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,700;1,300;1,700&display=swap" rel="stylesheet" />
